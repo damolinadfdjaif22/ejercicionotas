812239c9eba9e3615875fef07fd9a2c6814390ab
damolinadfdjaif22 <damolina23@espe.edu.ec>
2025-05-23 22:43:12 -0500

    calculo del promedio y console. log añadidos

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// Crear valores aleatorios para los exámenes
+// Crear valores para los exámenes
 let nota1 = 13;
 let nota2 = 11;
 let trabajoFinal = 20;
+
+// Calcular el promedio ponderado final
+ler promedio = (nota1 * 0.3) + (nota2 * 0.3) + (trabajoFinal * 0.4);
+
+// Mostrar las notas y el resultado final
+console.log("Nota 1:", nota1);
+console.log("Nota 2:", nota2);
+console.log("Trabajo Final:", trabajoFinal);
+console.log("Promedio final:", promedio.toFixed(2));
